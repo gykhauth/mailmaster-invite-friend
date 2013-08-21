7edda9ab302bd6e0269b89d1609299edfcd63e45
Khauth György <gykhauth@emesz.hu>
2013-08-21 16:52:56 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-mailmaster-invite-friend
-========================
+Hívd meg ismerőseid űrlap MailMaster segítségével
 
-Hívd meg ismerőseid űrlap (MailMaster)
+## Fájlok
+ajanlas-oldal.html : a továbbajánló űrlap
+ajanlas-feldolgozo.php : a továbbajánlás elmentését végző szkript.
+
+## Leírás az üzembe helyezéshez
+A továbbajánló rendszerhez két email listát kell létrehozni. 
+1. Az elsõ listában kerülnek eltárolásra az ajánló adatai (név, email cím és az ajánlások száma). 
+A mellékelt szkript alapján a következõ mezõket kell felvenni a listába (az email cím már alapértelmezetten benne van a listában):
+keresztnév - mssys_firstname
+ajánlások száma - nominee_num
+A listához létre kell hozni egy feliratkozó ûrlapot is. A lista és az ûrlap azonosítóját a szkript beállítások részében be kell állítani (NOMINATOR_LISTID és NOMINATOR_FORMID konstansok).
+
+2. A második listában kerülnek eltárolsára az ajánlottak adatai (név, email cím, ajánlás szövege, ajánló email címe, ajánló keresztneve).
+A mellékelt szkript alapján a következõ mezõket kell felvenni a listába (az email cím már alapértelmezetten benne van a listában):
+ajánlott keresztneve - mssys_firstname
+ajánlás szövege - email_content
+ajánló email címe - nominator_email
+ajánló keresztneve - nominator_firstname
+érvényes ajánlások száma - nominee_num
+Ehhez a listához is létre kell hozni egy feliratkozó ûrlapot. A lista és az ûrlap azonosítóját a szkript beállítások részében be kell állítani (NOMINEE_LISTID és NOMINEE_FORMID konstansok).
+
+Ha ez meg van, akkor létre kell hozni az ajánlásokat lehetõvé tevõ oldalt az ûrlappal. A szkript jelenleg maximum 4 ajánlást tud kezelni, ez tetszõleges számúra növelhetõ.
+Az az ajánlottak keresztnevét és email címét lehet megadni az ûrlapon illetve a szöveget, amelyet email-ben megkapnak.
+Az ajánlottak email címei az nominee_email_[i], keresztnevük az nominee_firstname_[i] konvenció alapján elnevezett mezõkben kell legyen megadva.
+A továbbajánló szöveg az email_content nevû mezõben szerepeljen. A továbbajánló szövegnek a hatályos jogszabály szerint szerkeszthetõnek kell lennie, azaz az ajánló át kell tudni írja.
+A szövegben szerepelnek olyan speciális mezõkódok amelyek kiküldéskor lesznek lecserélve, hogy az ajánló üzenet személyre legyen szabva. Az ajánlottak adatait tartalmazó lista mezõit lehet felhasználni a szövegben.
+Például: 
+Szia [mssys_firstname]! 
+Mindenképp nézd meg ezt a weboldalt .... 
+[nominator_firstname]
+
+A fenti szöveg az alábbi szöveggel fog elküldésre kerülni ha az ajánló keresztneve Gyuri, az ajánlotté Béla.
+Szia Béla!
+Mindenképp nézd meg ezt a weboldalt...
+Gyuri
+
+Az ajánlottak adatait tartalmazó listához létre kell hozni egy levelet, amelynek tartalma az [email_content] legyen. A szöveg elhelyezhetõ egy HTML levélben is természetesen. 
+Ez mindig az adott céltól függ. Elképzelhetõ olyan helyzet, hogy nincs használatban a továbbajánlás szövege (nem kötelezõ az ûrlapra sem kitenni), hanem egy sablon levél megy ki az ajánlottaknak, amely arra kéri õket, hogy erõsítsék meg a feliratkozásukat. A levélben ebben az esetben a kettõs opt-in megerõsítés kérõ kódot kell elhelyezni és az ajánlottakat gyûjtõ lista feliratkozó ûrlapjának feliratkozási módját kettõs opt-in-re kell állítani.
+
+Létre kell hozni egy feliratkozáskori (vagy kettõs opt-in feliratkozáskori) idõzítést a levélhez. Az idõzítés beállításánál lehetõség van arra, hogy a levél feladója az ajánló legyen. A feladó email címénél a [nominator_email], a feladó nevénél a [nominator_firstname] értéket kell megadni. Az idõzítés kiküldésekor a mezõkódok lecserélésre kerülnek az ajánló adataira.
---
+++ ajanlas-feldolgozo.php
@@ -0,0 +1,95 @@
+﻿<?php
+/**
+* Továbbajánló űrlapot feldolgozó szkript. Az input adatokat _POST metódussal, utf-8 kódolással kell átadni.
+*/
+
+/** Beállítások START **/
+define('MM_API_USERNAME',''); // A MailMaster fiókhoz tartozó API felhasználónév
+define('MM_API_PASSWORD',''); // A MailMaster fiókhoz tartozó API jelszó
+define('NOMINATOR_LISTID',0); // Az ajánlókat gyűjtő lista azonosítója
+define('NOMINATOR_FORMID',0); // Az ajánlókat gyűjtő lista egy feliratkozó űrlapjának azonosítója
+define('NOMINEE_LISTID',0); // Az ajánlottakat gyűjtő lista azonosítója
+define('NOMINEE_FORMID',0); // Az ajánlottakat gyűjtő lista egy feliratkozó űrlapjának azonosítója
+/** Beállítások END **/
+
+/**
+* Először elmentésre kerül az ajánló egy az ajánlókat gyűjtő email listába
+*/
+if ($_POST['email'] != '') {
+	$mmAPI = new mailmasterapi(NOMINATOR_LISTID,NOMINATOR_FORMID,MM_API_USERNAME,MM_API_PASSWORD);
+	unset($param);
+	$param = array(
+	'email' => $_POST['email'],
+	'mssys_firstname' => mb_convert_encoding($_POST['mssys_firstname'],'utf-8','iso-8859-2'),
+	'mssys_referer_page' => $_SERVER['HTTP_REFERER'] // Elmentésre kerül melyik oldalról iratkozott fel
+	);
+	$data = json_encode($param);
+	$headers = array(
+		'Accept: application/json',
+		'Content-Type: application/json'
+	);
+	$handle = curl_init();
+	curl_setopt($handle, CURLOPT_URL, 'http://'.MM_API_USERNAME.':'.MM_API_PASSWORD.'@restapi.emesz.com/subscribe/'.NOMINATOR_LISTID.'/form/'.NOMINATOR_FORMID);
+	curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($handle, CURLOPT_USERAGENT, 'MailMaster API');
+	curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($handle, CURLOPT_POST, true);
+	curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+	$nominatorID = curl_exec($handle);
+
+	if ($nominatorID > 0) {
+		/* Az ajánlott emberek adatai elmentésre kerülnek az ajánlottakat gyűjtő email listába. Feltételezzük, hogy az űrlapon maximum négy ajánlást lehet egyszerre leadni.
+		* Az az ajánlottak keresztneve és email címét lehet megadni az űrlapon illetve a szöveget, amelyet email-ben megkapnak.
+		* Az ajánlottak email címei az nominee_email_[i], keresztnevük az nominee_firstname_[i] konvenció alapján elnevezett mezőkben kell legyen megadva.
+		* A továbbajánló szöveg az email_content nevű mezőben szerepeljen.
+		*/
+		//$mmAPI = new mailmasterapi(NOMINEE_LISTID,NOMINEE_FORMID,MM_API_USERNAME,MM_API_PASSWORD);
+		//var $validNominees = 0;
+		for ($i = 1;$i <= 4;$i++) {
+			if ($_POST['nominee_email_'.$i] != '') {
+				// Ha az i. ajánlott email címe ki van töltve, akkor elmentésre kerül az ajánlást
+				$emailContent = preg_replace('/\[mssys_firstname\]/',$_POST['nominee_firstname_'.$i],$_POST['email_content']);
+				$emailContent = preg_replace('/\[nominator_firstname\]/',$_POST['mssys_firstname'],$emailContent);
+				
+				unset($param);
+				$param = array(
+				'email' => $_POST['nominee_email_'.$i],
+				'mssys_firstname' => mb_convert_encoding($_POST['nominee_firstname_'.$i],'utf-8','iso-8859-2'),
+				'email_content' => mb_convert_encoding($emailContent,'utf-8','iso-8859-2'),
+				'nominator_email' => $_POST['email'], // Ajánló email címe
+				'nominator_firstname' => mb_convert_encoding($_POST['mssys_firstname'],'utf-8','iso-8859-2'), // Ajánló keresztneve
+				'mssys_referer_page' => $_SERVER['HTTP_REFERER'] // Elmentésre kerül melyik oldalról iratkozott fel
+				);
+				$data = json_encode($param);
+				$headers = array(
+					'Accept: application/json',
+					'Content-Type: application/json'
+				);
+				$handle = curl_init();
+				curl_setopt($handle, CURLOPT_URL, 'http://'.MM_API_USERNAME.':'.MM_API_PASSWORD.'@restapi.emesz.com/subscribe/'.NOMINEE_LISTID.'/form/'.NOMINEE_FORMID);
+				curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($handle, CURLOPT_USERAGENT, 'MailMaster API');
+				curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
+				curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($handle, CURLOPT_POST, true);
+				curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+				$response = curl_exec($handle);
+
+				$validNominees++;
+			}
+
+			// Visszarögzítésre kerül az ajánlókat gyűjtő listába, hogy az illető hány ajánlást adott
+			if ($validNominees > 0) {
+				$mmAPI = new mailmasterapi(NOMINATOR_LISTID,NOMINATOR_FORMID,MM_API_USERNAME,MM_API_PASSWORD);
+				$mmAPI->update($nominatorID, array('nominee_num' => $validNominees));
+			}
+		}
+	}
+}
+?>
+<script>
+document.location.href = 'ajanlas-koszono.html';
+</script>
